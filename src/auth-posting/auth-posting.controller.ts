da7b67034d1dbb1d6effc47f5aaa0fae2705a247
jaehoon0 <choco730522@gmail.com>
2021-05-18 15:31:00 +0900

    add(authPosting/getPosting): 글 개수도 가져오도록 수정

--- src/auth-posting/auth-posting.controller.ts
+++ src/auth-posting/auth-posting.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Post, UseGuards, Request, Get, Query, Param } from '@
 import { AuthPostingService } from './auth-posting.service';
 import { AuthPosting } from '../entities/authPosting.entity'
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { PostingInfo } from '../entities/postingInfo'
 
 @Controller('auth-posting')
 export class AuthPostingController {
@@ -16,8 +17,13 @@ export class AuthPostingController {
 	}
 
 	@Get('postings/:type/:index')
-	async getPostings(@Param('type') type: string, @Param('index') index: number): Promise<AuthPosting[]> {
-		return await this.a_pService.getPostings(type, index);
+	async getPostings(@Param('type') type: string, @Param('index') index: number): Promise<PostingInfo> {
+		const postingArr = await this.a_pService.getPostings(type, index);
+		const postingInfo: PostingInfo = {
+			postings: postingArr,
+			num: postingArr.length
+		}
+		return postingInfo;
 	}
 
 }
---
+++ src/entities/postingInfo.ts
@@ -0,0 +1,6 @@
+import { AuthPosting } from './authPosting.entity'
+
+export class PostingInfo {
+	postings: AuthPosting[];
+	num: number
+}

f392f78b3ddf110b4e4bfa1d30de8c8e8170c704
jaehoon0 <choco730522@gmail.com>
2021-05-19 02:13:20 +0900

    edit(entities/user): 점수 관련 컬럼 추가

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,4 +1,5 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { ColumnMetadata } from 'typeorm/metadata/ColumnMetadata';
 
 @Entity()
 export class User {
@@ -16,4 +17,10 @@ export class User {
 
     @Column()
     location: string;
+
+    @Column({default: 0})
+    exp: number;
+
+    @Column({default: 50})
+    token: number;
 }
